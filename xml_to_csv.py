abd9004182224d33a24dda0c0a29900296a03be8
my name <email@example.com>
2021-03-31 10:58:29 +0700

    add test images

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -1,7 +1,7 @@
 """
 Usage:
 # Create train data:
-python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/train -o [PATH_TO_ANNOTATIONS_FOLDER]/train_labels.csv
+conython xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/train -o [PATH_TO_ANNOTATIONS_FOLDER]/train_labels.csv
 
 # Create test data:
 python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/test -o [PATH_TO_ANNOTATIONS_FOLDER]/test_labels.csv
